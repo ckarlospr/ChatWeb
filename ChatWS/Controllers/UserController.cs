29b4d2f4c476cb3fb4d60a1949aac33f061a8932
Carlos Perez <per.ckarlos@gmail.com>
2023-05-08 20:28:30 -0500

    adding a wep api's chat project

---
+++ ChatWS/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using ChatWS.Models.WS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChatWS.Controllers
+{
+    public class UserController : ApiController
+    {
+        public Reply HolaMundo()
+        {
+            Reply oReply = new Reply();
+            oReply.result = 1;
+            oReply.message = "Hola Mundo";
+
+
+            return oReply;
+        }
+    }
+}
